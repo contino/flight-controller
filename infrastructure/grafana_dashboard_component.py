41d1a96080b5a22ca75fbd82ef837423e1a7bd28
mayuthombre <mayu.thombre@contino.io>
2023-02-06 11:12:39 +1100

    grafana key

--- infrastructure/grafana_dashboard_component.py
+++ infrastructure/grafana_dashboard_component.py
@@ -1,34 +1,9 @@
-import json
 
-# import requests
-
-# # Set up the API URL and authentication credentials
-# url = "https://g-2e637a6ddc.grafana-workspace.ap-southeast-2.amazonaws.com/api/dashboards/db"
-# headers = {
-#     # "Content-Type": "application/json",
-#     # "Accept": "application/json",
-#     "Authorization": "Bearer eyJrIjoiekNIV2hqbzdtUVMzcmhGZVNaYXJXNmNBZ2w0ZDhDcnMiLCJuIjoiYWRtaW4iLCJpZCI6MX0="
-# }
-
-
-# # Load the dashboard configuration from a JSON file
-# with open("dashboard.json", "r") as f:
-#     data = json.load(f)
-
-
-# # Send the API request to create the dashboard
-# response = requests.post(url=url, headers=headers, json=data)
-
-# # Check the response status code
-# if response.status_code != 200:
-#     print("Error creating dashboard:", response.content)
-# else:
-#     print("Dashboard created successfully")
 
 # Import the necessary modules
 from constructs import Construct
+import json
 
-# from cdktf import TerraformStack
 from imports.grafana.folder import Folder
 from imports.grafana.dashboard import Dashboard
 from imports.grafana.provider import GrafanaProvider
@@ -48,7 +23,7 @@ class GrafanaDashboardComponent(Construct):
         self,
         scope: Construct,
         id: str,
-        grafana_workspace_api_key: grafana_workspace_api_key.GrafanaWorkspaceApiKey,
+        # grafana_workspace_api_key: grafana_workspace_api_key.GrafanaWorkspaceApiKey,
     ):
         super().__init__(scope, id)
 
