3d614f97fc6e5092585894806ea198bf74f8612e
mayuthombre <mayu.thombre@contino.io>
2023-02-09 16:01:15 +1100

    added tests.drivers for 'accounts'

--- tests/drivers/test_dynamo_event_sink.py
+++ tests/drivers/test_dynamo_event_sink.py
@@ -6,6 +6,94 @@ from uuid import UUID, uuid4
 import pytest
 
 from src.drivers.dynamo_event_sink_source import DynamoEventSink, DynamoEventSource
+
+from src.entities.projects import (
+    ProjectCreated,
+    ProjectCreatedPayload,
+    ProjectRequested,
+    ProjectRequestedPayload,
+)
+
+from src.entities.accounts import (
+    AccountCreated,
+    AccountCreatedPayload,
+    AccountRequested,
+    AccountRequestedPayload,
+)
+
+
+@pytest.mark.integration
+def test_retrieves_project_events_from_dynamodb():
+    aggregate_id = "".join(random.choices(string.ascii_letters, k=12))
+    projectRequested = ProjectRequested(
+        aggregateId=aggregate_id,
+        aggregateType="Project",
+        aggregateVersion=1,
+        eventId=uuid4(),
+        eventVersion=1,
+        payload=ProjectRequestedPayload(
+            aggregate_id, int(round(datetime.utcnow().timestamp()))
+        ),
+    )
+    projectCreated = ProjectCreated(
+        aggregateId=aggregate_id,
+        aggregateType="Project",
+        aggregateVersion=2,
+        eventId=uuid4(),
+        eventVersion=1,
+        payload=ProjectCreatedPayload(
+            aggregate_id, int(round(datetime.utcnow().timestamp()))
+        ),
+    )
+    sink = DynamoEventSink()
+    sink.store_events([projectRequested, projectCreated])
+
+    source = DynamoEventSource()
+
+    got = source.get_events_for_aggregate(projectRequested.aggregateId)
+
+    assert got == [projectRequested, projectCreated]
+
+
+@pytest.mark.integration
+def test_retrieves_account_events_from_dynamodb():
+    aggregate_id = "".join(random.choices(string.ascii_letters, k=12))
+    accountRequested = AccountRequested(
+        aggregateId=aggregate_id,
+        aggregateType="Account",
+        aggregateVersion=1,
+        eventId=uuid4(),
+        eventVersion=1,
+        payload=AccountRequestedPayload(
+            aggregate_id, int(round(datetime.utcnow().timestamp()))
+        ),
+    )
+    accountCreated = AccountCreated(
+        aggregateId=aggregate_id,
+        aggregateType="Account",
+        aggregateVersion=2,
+        eventId=uuid4(),
+        eventVersion=1,
+        payload=AccountCreatedPayload(
+            aggregate_id, int(round(datetime.utcnow().timestamp()))
+        ),
+    )
+    sink = DynamoEventSink()
+    sink.store_events([accountRequested, accountCreated])
+
+    source = DynamoEventSource()
+
+    got = source.get_events_for_aggregate(accountRequested.aggregateId)
+
+    assert got == [accountRequested, accountCreated]
+import random
+import string
+from datetime import datetime
+from uuid import UUID, uuid4
+
+import pytest
+
+from src.drivers.dynamo_event_sink_source import DynamoEventSink, DynamoEventSource
 from src.entities.compliance import (
     ResourceFoundCompliant,
     ResourceFoundCompliantPayload,
--- tests/drivers/test_timestream_metric_sink.py
+++ tests/drivers/test_timestream_metric_sink.py
@@ -2,6 +2,28 @@ import pytest
 import random
 
 from src.drivers.timestream_metric_sink import TimeStreamMetricSink
+
+from src.entities.projects import ProjectLeadTime
+from src.entities.accounts import AccountLeadTime
+
+@pytest.mark.integration
+def test_store_metrics_does_not_return_error():
+    aggregate_id = "testMetric"
+    lead_time = random.randint(0, 7200)
+    sink = TimeStreamMetricSink()
+    assert (
+        sink.store_metrics(
+            [
+                ProjectLeadTime(aggregate_id, lead_time),
+            ]
+        )
+        is None
+    )
+
+import pytest
+import random
+
+from src.drivers.timestream_metric_sink import TimeStreamMetricSink
 from src.entities.compliance import ResourceComplianceLeadTime
 from src.entities.projects import ProjectLeadTime
 
@@ -30,6 +52,20 @@ def test_store_resource_compliance_lead_time_does_not_return_error():
         sink.store_metrics(
             [
                 ResourceComplianceLeadTime(aggregate_id, lead_time),
+            ]
+        )
+        is None
+    )
+
+@pytest.mark.integration
+def test_store_metrics_does_not_return_error():
+    aggregate_id = "testMetric"
+    lead_time = random.randint(0, 7200)
+    sink = TimeStreamMetricSink()
+    assert (
+        sink.store_metrics(
+            [
+                AccountLeadTime(aggregate_id, lead_time),
             ]
         )
         is None
