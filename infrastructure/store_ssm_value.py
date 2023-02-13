50b226e14754d9d9381c77f260dc19e0638da8ed
mayuthombre <mayu.thombre@contino.io>
2023-02-14 09:31:42 +1100

    delete ssm store python file (redundant)

--- infrastructure/store_ssm_value.py
+++
@@ -1,9 +0,0 @@
-# Store API Key valie in SSM Parameter store
-import boto3
-ssm_client = boto3.client('ssm')
-ssm_client.put_parameter(
-    Name="flight_controller_api_key", 
-    Value="<INSERT NEW KEY HERE>", 
-    Type="SecureString", 
-    Overwrite=True,
-    )
