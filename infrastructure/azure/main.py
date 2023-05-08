bb25800878f52e6024db31c709688e5dcec74641
dstojanovski-contino <david.stojanovski@contino.io>
2023-05-08 18:34:16 +1000

    Added build targets

--- infrastructure/azure/main.py
+++ infrastructure/azure/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 from cdktf import App, TerraformStack, TerraformOutput
-from cdktf_cdktf_provider_azure.provider import AzurermProvider
+from cdktf_cdktf_provider_azurerm.provider import AzurermProvider
 from constructs import Construct
 
 class AzureCore(TerraformStack):
@@ -14,12 +14,7 @@ class AzureCore(TerraformStack):
 
         AzurermProvider(self, "Azure")
 
-        self.dynamotable_name = "event_sourcing_table"
-        self.lambda_name = "producer_lambda_function_cdktf"
-        self.event_bridge_name = "main_lambda_bus_cdktf"
-        self.timestream_db_name = "core_timestream_db"
-        self.grafana_workspace_name = "grafana_dashboard"
-        self.grafana_lambda_name = "grafana_api_key_rotator"
+        self.resource_group = "rg_name"
 
 app = App()
 core_stack = AzureCore(app, "azure_core")
