4bfa4f1c2cc1c2372ede8b638b67518f457dc31b
dstojanovski-contino <david.stojanovski@contino.io>
2023-05-08 19:04:53 +1000

    Config fix + grafana dashboard

--- infrastructure/azure/main.py
+++ infrastructure/azure/main.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
+from constructs import Construct
 from cdktf import App, TerraformStack, TerraformOutput
 from cdktf_cdktf_provider_azurerm.provider import AzurermProvider
-from constructs import Construct
+from imports.grafana.provider import GrafanaProvider
+from grafana_dashboard_component import GrafanaDashboardComponent
 
 class AzureCore(TerraformStack):
     def __init__(
@@ -11,11 +13,42 @@ class AzureCore(TerraformStack):
         id: str,
     ):
         super().__init__(scope, id)
+        self.resource_group = "flight_controller"
+        self.azure_function = "azfcdev"
+        self.azure_sql = "azfcdevdb"
 
-        AzurermProvider(self, "Azure")
+        AzurermProvider(
+            self, 
+            "Azure", 
+            features = {}
+        )        
 
-        self.resource_group = "rg_name"
+class Azurerafana(TerraformStack):
+    def __init__(self, scope: Construct, id: str, workspace_id: str):
+        super().__init__(scope, id)
+        
+        AzurermProvider(
+            self, 
+            "Azure", 
+            features = {}
+        )
+
+        GrafanaProvider(
+            self,
+            "Grafana",
+            auth=api_key.secret_string,
+            url="https://"
+            + workspace_id
+            + ".azurewebsites.net",
+        )
+
+        GrafanaDashboardComponent(
+            self,
+            "grafana_dashboard"
+        )
+
 
 app = App()
 core_stack = AzureCore(app, "azure_core")
+grafana_dashboard_stack = AzureGrafana(app, "azure_grafana_dashboard", core_stack.grafana_workspace_id)
 app.synth()
