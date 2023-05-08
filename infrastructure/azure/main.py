91046a5ead60d2b54d3f63fb7741d38577bd7039
dstojanovski-contino <david.stojanovski@contino.io>
2023-05-08 19:29:20 +1000

    Added Azure resources

--- infrastructure/azure/main.py
+++ infrastructure/azure/main.py
@@ -5,6 +5,10 @@ from cdktf import App, TerraformStack, TerraformOutput
 from cdktf_cdktf_provider_azurerm.provider import AzurermProvider
 from imports.grafana.provider import GrafanaProvider
 from grafana_dashboard_component import GrafanaDashboardComponent
+from cdktf_cdktf_provider_azurerm import (
+    resource_group
+)
+
 
 class AzureCore(TerraformStack):
     def __init__(
@@ -13,17 +17,29 @@ class AzureCore(TerraformStack):
         id: str,
     ):
         super().__init__(scope, id)
-        self.resource_group = "flight_controller"
-        self.azure_function = "azfcdev"
+
+        self.location = "australiasoutheast"
+        self.resource_group_name = "flight_controller"
+        self.function_name = "azfcdev"
         self.azure_sql = "azfcdevdb"
+        self.grafana_workspace_name = "grafana_dashboard"
 
         AzurermProvider(
             self, 
             "Azure", 
             features = {}
-        )        
+        )
 
-class Azurerafana(TerraformStack):
+        resourceGroup = resource_group.ResourceGroup(
+            self, 
+            "rg",
+            name = self.resource_group_name,
+            location = self.location,
+        )
+        
+        # self.grafana_workspace_id = grafanaWorkspace.grafana_workspace.id
+
+class AzureGrafana(TerraformStack):
     def __init__(self, scope: Construct, id: str, workspace_id: str):
         super().__init__(scope, id)
         
@@ -50,5 +66,5 @@ class Azurerafana(TerraformStack):
 
 app = App()
 core_stack = AzureCore(app, "azure_core")
-grafana_dashboard_stack = AzureGrafana(app, "azure_grafana_dashboard", core_stack.grafana_workspace_id)
+#grafana_dashboard_stack = AzureGrafana(app, "azure_grafana_dashboard", core_stack.grafana_workspace_id)
 app.synth()
