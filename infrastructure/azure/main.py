902e7fddda3fb4d4c1f5c0f66228d8eb8bcd28a0
David Stojanovski <david.stojanovski@contino.io>
2023-05-21 18:25:17 +1000

    Grafana

--- infrastructure/azure/main.py
+++ infrastructure/azure/main.py
@@ -12,7 +12,8 @@ from cdktf_cdktf_provider_azurerm import (
     mssql_database,
     service_plan,
     linux_function_app,
-    eventgrid_topic
+    eventgrid_topic,
+    dashboard_grafana
 )
 from imports.grafana.provider import GrafanaProvider
 from imports.grafana.folder import Folder
@@ -34,28 +35,28 @@ class AzureCore(TerraformStack):
         with open("azurerm.json", "r") as f:
             az = json.load(f)
 
-        rg = resource_group.ResourceGroup(
+        self.rg = resource_group.ResourceGroup(
             self, 
             az['resource_group']['key'],
             name      = f"{az['key']}{az['env']}{az['resource_group']['key']}",
             location  = az['location']
         )
 
-        sa = storage_account.StorageAccount(
+        self.sa = storage_account.StorageAccount(
             self,
             az['storage_account']['key'],
             location                   = az['location'],
             name                       = f"{az['key']}{az['env']}{az['storage_account']['key']}",
             account_tier               = az['storage_account']['tier'],
             account_replication_type   = az['storage_account']['replication'],
-            resource_group_name        = rg.name
+            resource_group_name        = self.rg.name
         )
 
         sc = storage_container.StorageContainer(
             self, 
             az['storage_container']['key'],
             name = f"{az['key']}{az['env']}{az['storage_container']['key']}",
-            storage_account_name = sa.name
+            storage_account_name = self.sa.name
         )
 
         sql = mssql_server.Mssql(
@@ -66,7 +67,7 @@ class AzureCore(TerraformStack):
             version  = az['mssql_server']['version'],
             administrator_login = az['mssql_server']['administrator_login'],
             administrator_login_password = az['mssql_server']['administrator_login_password'],
-            resource_group_name = rg.name
+            resource_group_name = self.rg.name
         )
 
         db = mssql_database.MssqlDatabase(
@@ -79,37 +80,49 @@ class AzureCore(TerraformStack):
             server_id = sql.id
         )
 
-        svc = service_plan.ServicePlan(
+        self.svc = service_plan.ServicePlan(
             self, 
             az['service_plan']['key'],
             location = az['location'],
             name = f"{az['key']}{az['env']}{az['service_plan']['key']}",
             sku_name = az['service_plan']['sku_name'],
             os_type = az['service_plan']['os_type'],
-            resource_group_name = rg.name
+            resource_group_name = self.rg.name
         )
 
-        fnc = linux_function_app.LinuxFunctionApp(
+        self.fnc = linux_function_app.LinuxFunctionApp(
             self,
             az['linux_function_app']['key'],
             name = f"{az['key']}{az['env']}{az['linux_function_app']['key']}",
             location = az['location'],
-            resource_group_name = rg.name,
+            resource_group_name = self.rg.name,
             service_plan_id = svc.id,
-            storage_account_name = sa.name,
-            storage_account_access_key = sa.primary_access_key,
+            storage_account_name = self.sa.name,
+            storage_account_access_key = self.sa.primary_access_key,
             site_config = { },
             identity = { "type": "SystemAssigned" }
         )
 
-        eventgridtopic = eventgrid_topic.EventgridTopic(
+        self.eventgridtopic = eventgrid_topic.EventgridTopic(
             self,
             az['eventgrid_topic']['key'],
             location = az['location'],
-            resource_group_name = rg.name,
-            name = f"{az['key']}{az['env']}egt"
+            resource_group_name = self.rg.name,
+            name = f"{az['key']}{az['env']}{az['eventgrid_topic']['key']}"
         )
 
+        self.dashboard = dashboard_grafana.DashboardGrafana(
+            self,
+            az['dashboard_grafana']['key'],
+            location = az['location'],
+            resource_group_name = self.rg.name,
+            name = f"{az['key']}{az['env']}{az['dashboard_grafana']['key']}",
+            api_key_enabled = True
+            deterministic_outbound_ip_enabled = True
+            public_network_access_enabled = False
+            identity = { "type": "SystemAssigned" }
+        )
+       
 class AzureGrafana(TerraformStack):
     def __init__(self, scope: Construct, id: str, workspace_id: str):
         super().__init__(scope, id)
@@ -123,6 +136,7 @@ class AzureGrafana(TerraformStack):
         self.grafana = {
             'auth': 'supersecretkeygoeshere',
             'url': 'https://apacfcdevgrafana.azurewebsites.net'
+            'url': 'https://apacfcdevgrad.azurewebsites.net'
         }
 
         GrafanaProvider(
@@ -152,5 +166,5 @@ class AzureGrafana(TerraformStack):
 
 app = App()
 core_stack = AzureCore(app, "azure_core")
-#grafana_dashboard_stack = AzureGrafana(app, "azure_grafana_dashboard", core_stack.grafana_workspace_id)
+grafana_dashboard_stack = AzureGrafana(app, "azure_grafana_dashboard", core_stack.dashboard.)
 app.synth()
